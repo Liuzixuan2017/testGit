53d798687206568c6ac0303f6b0279b6cb0283b3
Zixuan Liu <liuzixuan199508@gmail.com>
2018-04-09 11:31:48 -0400

    add substract functions

--- calculator.java
+++ calculator.java
@@ -2,11 +2,20 @@ class calculator{
 	public int add(int x, int y){
 		return x+y;
 	}
-	public double add(double x, double y){
+	public double add(int x, int y){
 		return x+y;
 	}
 	public float add(float x, float y) {
 		return x+y;
+	}
+	public int substract(int x, int y) {
+		return x-y;
+	}
+	public double substract(double x, double y) {
+		return x-y;
+	}
+	public float substract(float x, float y) {
+		return x-y;
 	}
 	public static void main(String[] args) {
 		
