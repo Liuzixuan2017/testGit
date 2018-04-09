fc5538475f051a71386da9f5ee834af14915d9e9
Zixuan Liu <liuzixuan199508@gmail.com>
2018-04-09 10:26:00 -0400

    add add functions for int, float and double

--- calculator.java
+++ calculator.java
@@ -1,5 +1,14 @@
 class calculator{
-
-
-
+	public int add(int x, int y){
+		return x+y;
+	}
+	public double add(double x, double y){
+		return x+y;
+	}
+	public float add(float x, float y) {
+		return x+y;
+	}
+	public static void main(String[] args) {
+		
+	}
 }
